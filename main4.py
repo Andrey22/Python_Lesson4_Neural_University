c0c958b262dcb2fe6aac553176a55479ab1d9f35
andrey.andreyko <aandreyko@mail.ru>
2020-01-23 18:20:43 +0300

    Commit4

---
+++ main4.py
@@ -0,0 +1,18 @@
+import random
+# 1. Напишите функцию (F): на вход список имен и целое число N; на выходе список длины N случайных имен из первого списка (могут повторяться,
+# можно взять значения: количество имен 20, N = 100,
+# рекомендуется использовать функцию random);
+
+def choisename(*args, N):
+    for i in range(101):
+        index=random.randint(i)
+        list.append(list2,list1(index))
+    return list2
+
+list3=['Bdfy','Ivan']
+
+# list2=choisename(list3, 100)
+N=100
+choisename(list3, 100)
+
+print (list2)
