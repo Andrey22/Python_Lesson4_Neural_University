ed5abf8cefd1a5a4abd17fbe0345d51b94f982c9
Andrey22 <aandreyko@mail.ru>
2020-01-24 02:31:20 +0300

    Commit4

--- main4.py
+++ main4.py
@@ -1,34 +1,12 @@
-import random
-from random import choices
 # 1. Напишите функцию (F): на вход список имен и целое число N; на выходе список длины N случайных имен из первого списка (могут повторяться,
 # можно взять значения: количество имен 20, N = 100,
 # рекомендуется использовать функцию random);
-# def choisename(list3, N):
-# # for i in range(N):
-#     index=random.randint(N)
-#     list.append(list2,list1(index))
-#     return list2
-#
-# list3=['Alex','Ivan', 'Sveta', 'Sima']
-# print (list3)
-#
-# # list2=choisename(['Alex','Ivan', 'Sveta', 'Sima'],8)
-#
-# N=8
-# list2=choisename(list3, N)
-#
-# print (list2)
-#
-# from random import choices
+import random
+def choice_name(names, lenth):
+    return random.choices(names, k=lenth)
 
+listnames=['Kate', 'John', 'Nataly', 'Liza', 'John', 'Andrew', 'Olga', 'Maria', 'Abraham', 'Julia', 'Ornella', 'Liza', 'Maria', 'Alex', 'Peter', 'Julia', 'Andrew', 'Kate', 'Samuil', 'Galina']
 
+new_list = choice_name(listnames, lenth=100)
 
-def choice_name(names, len_list):
-
-    return 1
-
-
-list1=['Kate', 'Adam', 'Nataly', 'Lisa', 'John', 'Kate', 'Olga', 'Maria', 'Artem', 'Julia', 'Olga', 'Lisa', 'Maria', 'Alex', 'Peter', 'Julia', 'Andrew', 'Kate', 'Sam', 'Garry']
-my_list = choice_name(list1, len_list=100)
-
-print(my_list)
+print(new_list)
