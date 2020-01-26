1acf09ebcb2da47187ce0c014ae1718b5e9ebf4e
Andrey22 <aandreyko@mail.ru>
2020-01-26 22:53:32 +0300

    Commit 4.1

--- main4.py
+++ main4.py
@@ -19,6 +19,21 @@ def frequentname(listname):
         dict[name] = dict.get(name,0) + 1
     dict = list(dict.items())
     dict.sort(key=lambda x: x[1], reverse=True)
-    return dict[0]
+    # x[1]  означает сортировку по Values, т.е. по количеству повторений имени в словаре (имя, количество повторений)
+    return dict[0][0]
 
 print(frequentname(new_list))
+
+# 3. Напишите функцию вывода самой редкой буквы, с которого начинаются имена в списке на выходе функции F.
+
+def rareletter(list2):
+    dict2 = {}
+    for name in list2:
+        for char in name:
+            dict2[char] = dict2.get(char,0) + 1
+    dict2 = list(dict2.items())
+    dict2.sort(key=lambda x: x[1])
+    # x[1]  означает сортировку по Values, т.е. по количеству повторений имени в словаре (имя, количество повторений)
+    return dict2[0][0]
+print(rareletter(new_list))
+
